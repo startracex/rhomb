6a9cb2840737eddb81d355e59aade23d1e7b8d48
Shiro Wang <startraceebr@foxmail.com>
2026-01-30 21:15:28 +0800

    deps: update dependencies

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,12 +1,13 @@
+import { webdriverio } from "@vitest/browser-webdriverio";
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
     browser: {
       enabled: true,
-      provider: "webdriverio",
+      provider: webdriverio(),
+      // https://vitest.dev/config/browser/webdriverio
       instances: [{ browser: "chrome" }],
-      headless: true,
     },
   },
 });
